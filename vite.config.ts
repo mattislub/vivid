8ab646d9ece5e74c239337da62c7269e932d8180
mattislub <166994172+mattislub@users.noreply.github.com>
2025-09-14 13:15:00 +0300

    chore: set app and server ports

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 6000,
+    proxy: {
+      '/api': 'http://localhost:6001',
+    },
+  },
+  preview: {
+    port: 6000,
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
